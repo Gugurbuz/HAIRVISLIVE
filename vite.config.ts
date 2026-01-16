5e0e0ead4543d9425f6111dd0a1e2e9e4fb6a5ad
Gugurbuz <gurkangurbuz@hotmail.com.tr>
2026-01-16 19:47:33 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import compression from 'vite-plugin-compression';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
@@ -14,20 +13,6 @@ export default defineConfig(({ mode }) => {
 
     plugins: [
       react(),
-      compression({
-        verbose: true,
-        disable: false,
-        threshold: 10240,
-        algorithm: 'gzip',
-        ext: '.gz',
-      }),
-      compression({
-        verbose: true,
-        disable: false,
-        threshold: 10240,
-        algorithm: 'brotliCompress',
-        ext: '.br',
-      }),
     ],
 
     define: {
